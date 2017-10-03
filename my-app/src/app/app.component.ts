d0106deab884d871f8fee310804e5c991c223208
Pascal Grimaud <pascalgrimaud@gmail.com>
2017-10-03 15:11:30 +0200

    Add validation by code

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Item } from "./item";
+import { FormBuilder, FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,34 @@ import { Item } from "./item";
 })
 export class AppComponent implements OnInit {
   collection: Item[];
-  newItem: Item;
+  form: FormGroup;
+
+  nameCtrl: FormControl;
+  refCtrl: FormControl;
+  stateCtrl: FormControl;
+
+  constructor(fb: FormBuilder) {
+    // pour mettre plusieurs validateurs, mettre un tableau
+    this.nameCtrl = fb.control('', [
+      Validators.required,
+      Validators.minLength(2)
+    ]);
+    this.refCtrl = fb.control('', [
+      Validators.required,
+      Validators.minLength(4)
+    ])
+    this.stateCtrl = fb.control(0);
+
+    // name, reference: correspondent aux noms dans le
+    // formulaire html
+    this.form = fb.group({
+      name: this.nameCtrl,
+      ref: this.refCtrl,
+      state: this.stateCtrl
+    });
+    // il faut ensuite penser à ajouter [formGroup]="form"
+    // dans le formulaire html
+  }
 
   ngOnInit(): void {
       this.collection = [
@@ -16,15 +44,24 @@ export class AppComponent implements OnInit {
         new Item({reference: '2345', name: 'Patrick', state: 1}),
         new Item({reference: '3456', name: 'Quentin', state: 2})
       ];
-      this.resetNewItem();
   }
 
   addItem() {
-    this.collection.push(this.newItem);
-    this.resetNewItem();
+    console.log(this.form.value);
+    this.collection.push({
+      name: this.form.get('name').value,
+      reference: this.form.get('ref').value,
+      state: this.form.get('state').value
+    });
+    this.reset();
   }
 
-  resetNewItem() {
-    this.newItem = new Item({reference: '', name: '', state: 0});
+  reset() {
+    this.form.reset();
+    this.stateCtrl.setValue(0);
+  }
+
+  isChampValid(champs: string) {
+    return this.form.get(champs).dirty && this.form.get(champs).hasError('minlength');
   }
 }
