b0c4d82282c5b452b10c98dd0871cf79de50a933
Pascal Grimaud <pascalgrimaud@gmail.com>
2017-10-03 10:54:14 +0200

    Merge branch 'release/5-6'
    Add item component

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { ListItemComponent } from './list-item/list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    ListItemComponent
   ],
   imports: [
     BrowserModule
